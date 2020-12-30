7f273383c9a16fc20534cee00016b06d13d75be2
vvhanxing <33742233+vvhanxing@users.noreply.github.com>
2020-12-30 10:40:57 +0800

    Add files via upload

---
+++ upgrade/CreatObj.cs
@@ -0,0 +1,237 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatObj : MonoBehaviour
+{
+    // Start is called before the first frame update
+    RaycastHit hit;
+    //public GameObject parentObj; 
+    //public GameObject rayObj;
+    public GameObject creatObjsClass;
+    public GameObject[]  creatObjs = new GameObject[4];
+    public GameObject[]  creatObjs_layer1 = new GameObject[4];
+    public GameObject[]  creatObjs_layer2 = new GameObject[4];
+
+    private GameObject creatObj;
+   
+
+    public int Num = 20;
+    public GameObject mapObj ; //
+    public bool IsOcclusion =false;
+    public float mapRange = 50.0f;
+    public bool isDiv =false;
+    public int mapDiv = 5;
+    public float[] ScaleRange =  new float[2]{1.0f,2.0f};
+    public float distanceFromMap = 0.0f;
+    public float angle  =0.0f;
+    float RandomRangeX = 0.0f;
+    float RandomRangeZ = 0.0f;
+
+    void Start()
+    {
+        if (isDiv==true){
+            adding2();
+
+        }
+        
+        
+   
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDiv==false)
+        {
+
+            int NumCount = creatObjsClass.transform.childCount;
+            if (NumCount<Num )
+            {
+                adding(Num - NumCount);
+            }
+
+        }
+
+
+
+            
+
+
+
+       
+    }
+
+
+
+
+    private void adding(int addNum)
+    {
+    
+        for (int i = 0; i < addNum; i++) 
+        {
+            RandomRangeX = transform.position[0]+Random.Range(-mapRange,mapRange);
+            RandomRangeZ= transform.position[2]+Random.Range(-mapRange,mapRange);
+            Vector3 p = new Vector3(RandomRangeX,100.0f,RandomRangeZ);
+
+        
+
+            if (Physics.Raycast(p,-transform.up,out hit,200))
+        
+            {
+                if (IsOcclusion==true)
+                {
+
+                    if (hit.collider.name==mapObj.name)
+                    {
+                        creatObj = creatObjs [Random.Range(0,creatObjs.Length)];
+ 
+                        Instantiate(creatObj, new Vector3(hit.point[0],hit.point[1]+distanceFromMap,hit.point[2]),transform.rotation);
+
+                        creatObj.transform.rotation = Quaternion.Euler(0,0,0);
+
+                        float randomScale =  Random.Range(ScaleRange[0],ScaleRange[1]);
+                        creatObj.transform.localScale = new Vector3(randomScale,randomScale,randomScale);
+                    }
+
+                }
+
+                else
+                {
+                        creatObj = creatObjs [Random.Range(0,creatObjs.Length)];
+ 
+                        GameObject newCreatObj = Instantiate(creatObj, new Vector3(hit.point[0],hit.point[1]+distanceFromMap,hit.point[2]),transform.rotation);
+
+                        newCreatObj.transform.rotation = Quaternion.Euler(0,0,0);
+
+                        float randomScale =  Random.Range(ScaleRange[0],ScaleRange[1]);
+                        newCreatObj.transform.localScale = new Vector3(randomScale,randomScale,randomScale);
+                        newCreatObj.transform.parent = creatObjsClass.transform;
+
+                }
+
+ 
+
+            }
+
+        }        
+    }
+
+
+
+    private void adding2()
+    {
+        
+        int Px = (int)(2.0f*mapRange/mapDiv);
+        int Py = (int)(2.0f*mapRange/mapDiv);
+
+        for (int layer = 0; layer < 3 ;layer++) 
+        {
+            for (int i = 0; i < mapDiv; i++) 
+            {
+                for (int j = 0; j < mapDiv; j++) 
+                {
+                    float dPx = (i)*Px+0.5f*Px-mapRange;
+                    float dPy = (j)*Py+0.5f*Py-mapRange;
+                    Vector3 p = new Vector3(dPx+transform.position[0],100.0f,dPy+transform.position[2]);
+
+
+
+
+                    if (Physics.Raycast(p,-transform.up,out hit,200))
+        
+                    {
+
+                        
+                        if (layer == 0)
+                        {
+                             
+                            creatObj = creatObjs [Random.Range(0,creatObjs.Length)];
+                        }
+                         if (layer == 1)
+                        {
+                            //print(hit.collider.name);
+                            if (hit.collider.name!=mapObj.name )
+                            {
+
+                                creatObj = creatObjs_layer1[Random.Range(0,creatObjs_layer1.Length)];
+
+                            }
+                            else
+                            {
+                                creatObj =null;
+                            }
+                            
+                        }
+                        if (layer == 2)
+                        {
+                             
+                            //print(hit.collider.name);
+                            if (hit.collider.name!=mapObj.name )
+                            {
+
+                                creatObj = creatObjs_layer1[Random.Range(0,creatObjs_layer1.Length)];
+
+                            }
+                            else
+                            {
+                                creatObj =null;
+                            }
+                        }
+
+
+                        
+                        if (creatObj!=null)
+                        {
+                            GameObject newCreatObj = Instantiate(creatObj, new Vector3(hit.point[0],hit.point[1]+distanceFromMap,hit.point[2]),transform.rotation);
+
+                            newCreatObj.transform.rotation = Quaternion.Euler(0,0,0);
+
+                            float randomScale =  Random.Range(ScaleRange[0],ScaleRange[1]);
+                            newCreatObj.transform.localScale = new Vector3(randomScale,randomScale,randomScale);
+                            newCreatObj.transform.parent = creatObjsClass.transform;
+                        }
+
+
+ 
+
+                    }                
+
+
+                
+                
+
+                }
+            }
+        }
+    
+      
+    }
+
+
+
+
+
+
+
+
+
+    private void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.red;
+        Vector3 pointA = new Vector3(transform.position[0]+mapRange,transform.position[1],transform.position[2]+mapRange);
+        Vector3 pointB = new Vector3(transform.position[0]-mapRange,transform.position[1],transform.position[2]+mapRange);
+        Vector3 pointC = new Vector3(transform.position[0]+mapRange,transform.position[1],transform.position[2]-mapRange);
+        Vector3 pointD = new Vector3(transform.position[0]-mapRange,transform.position[1],transform.position[2]-mapRange);
+        Gizmos.DrawLine(pointA, pointB);
+        Gizmos.DrawLine(pointA, pointC);
+        Gizmos.DrawLine(pointB, pointD);
+        Gizmos.DrawLine(pointC, pointD);
+    }
+
+
+
+
+}
---
+++ upgrade/FindObjectByRay.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindObjectByRay : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //public string findObjTag;
+    public GameObject findObj; 
+    public float angleRange = 45.0f;
+    public float distanceRange = 20.0f;
+    public float giveUpDistance = 10.0f;
+    public float giveUpTime = 10f;
+    public float giveUpTimeCount = 0.0f;
+
+    public float findObjDistance =30.0f; //findObjDistance>giveUpDistance
+
+    public string[] FindObjsClassName = new string[1] ;
+
+    private Vector3 findObjDirection;
+
+    
+    private List<float> KnifeList;
+    private Dictionary<float, GameObject> knifeDic;
+
+    private float playerHight;
+    private Vector3 PlayerPoint ;
+    private Vector3 CapturePoint ;
+    //public DrawSector drawSector ;
+    private Vector3 newVecA;
+    private Vector3 newVecB;
+
+    
+
+    void Start()
+    {
+
+         //drawSector =GetComponent<DrawSector>();
+         playerHight = GetComponent<BoxCollider>().size.y*transform.localScale.y;
+        
+    }
+
+    void FixedUpdate()
+    {
+        PlayerPoint =new Vector3(transform.position[0],transform.position[1]+playerHight,transform.position[2]) ;
+
+
+        if (findObj==null) 
+        {
+            Finding() ;
+            
+            
+        }
+
+
+        else
+        {
+            CapturePoint = new Vector3(findObj.transform.position[0],transform.position[1]+playerHight,findObj.transform.position[2]);
+
+            findObjDirection = CapturePoint - PlayerPoint;
+            findObjDistance = findObjDirection.magnitude;
+            RaycastHit hitfindObj;
+
+            if (findObjDistance>giveUpDistance )
+            {
+                findObj=null;
+ 
+            }
+            
+            if (Physics.Raycast( PlayerPoint, findObjDirection , out hitfindObj,distanceRange))
+            {
+                if (hitfindObj.collider.gameObject != findObj)
+                {
+
+                    giveUpTimeCount +=Time.deltaTime;
+                    //print(giveUpTimeCount);
+                
+                    if (giveUpTimeCount>giveUpTime)
+                    {
+                        findObj=null;
+                        giveUpTimeCount = 0.0f;
+
+                    }
+
+                }
+
+            }
+
+
+        }
+        
+    }    
+
+
+
+
+
+
+    void Finding()
+    {
+
+        
+
+
+
+        knifeDic = new Dictionary<float, GameObject>();//初始化       
+        KnifeList = new List<float>();
+        foreach (string FindObjName in FindObjsClassName)
+        {
+            GameObject FindObjs = GameObject.Find(FindObjName);
+            for (int count =0;count < FindObjs.transform.childCount; count++)
+            //foreach (GameObject obj in GameObject.FindGameObjectsWithTag(findObjTag)) 
+            {
+                GameObject obj = FindObjs.transform.GetChild(count).gameObject;
+                Vector3 objDirection = obj.transform.position - PlayerPoint;
+                float distance =  objDirection.magnitude;
+
+                RaycastHit hit;
+
+                if (distance<=distanceRange)
+                {
+
+            
+                    if (Physics.Raycast( PlayerPoint,objDirection , out hit,distanceRange))
+                    {
+                        if (hit.collider.gameObject ==obj)
+                        {
+                            //Debug.DrawLine(transform.position, obj.transform.position, Color.red, 1);
+ 
+                            float angle = Vector3.Angle (transform.forward, objDirection);                    
+                    
+                            if (angle <=angleRange)
+                            {
+                            //print( string.Format("{0} {1} {2}",obj.name,distance,angle));
+                                Debug.DrawLine(transform.position, obj.transform.position, Color.blue, 1);
+
+                                knifeDic.Add(distance, obj);
+
+                                if (!KnifeList.Contains(distance))
+                                {
+                                    KnifeList.Add(distance);
+                                }
+                            }
+
+
+                        }
+                    
+                    
+                    }
+
+                }
+
+            }
+        }
+        KnifeList.Sort();
+        //print(string.Format("----> {0}",KnifeList));
+        if (KnifeList. Count!=0)knifeDic.TryGetValue(KnifeList[0],out findObj);
+        
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        newVecA = Quaternion.AngleAxis(angleRange,new Vector3(0,1,0))*transform.forward;
+        newVecB = Quaternion.AngleAxis(-angleRange,new Vector3(0,1,0))*transform.forward;
+
+        //Vector3 point = new Vector3(transform.position[0],transform.position[1]+playerHight,transform.position[2]);
+        
+        Gizmos.DrawLine(PlayerPoint, PlayerPoint+newVecA*distanceRange);
+        Gizmos.DrawLine(PlayerPoint, PlayerPoint+newVecB*distanceRange);
+    }
+
+}
