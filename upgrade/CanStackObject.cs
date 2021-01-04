05e979c32ff022e27bbbb54ca5eca0284c7e5362
vvhanxing <33742233+vvhanxing@users.noreply.github.com>
2021-01-04 20:40:44 +0800

    Update CanStackObject.cs

--- upgrade/CanStackObject.cs
+++ upgrade/CanStackObject.cs
@@ -7,14 +7,13 @@ public class CanStackObject : MonoBehaviour
     // Start is called before the first frame update
     
     public List<GameObject> StackObjectList = new List<GameObject>();
+    public GameObject branch;
     public float[]  itemScaleRange =new float[]{1,1};
 
     void Start()
     {
 
-    }
-    void Update () 
-    {
+
         //返回一条射线从摄像机通过一个屏幕点
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitInfo = new RaycastHit();
@@ -38,11 +37,27 @@ public class CanStackObject : MonoBehaviour
                 Transform child = transform.GetChild(i).gameObject.transform;
                 //Debug.Log(child.name);
                 
-
-                if (Physics.Raycast(child.position,child.forward,out hitInfo,10))
+                int layA = LayerMask.NameToLayer("Cube");
+                int layB = LayerMask.NameToLayer("Default");
+                if (Physics.Raycast(child.position,child.forward,out hitInfo,20,  (1 << layA) | (1 << layB)  ))
                 {
                     print(hitInfo.point);
+
                     Debug.DrawLine(child.position,hitInfo.point,Color.red);  //child.position+8* child.forward
+                    
+                    float creatCount = (child.position-hitInfo.point).magnitude;
+                    if (branch!=null)
+                    {
+
+                        for (int j= 0;j<3;j++)
+                        {
+                            Instantiate(branch,child.position+child.forward*j*2,child.rotation);
+                        }
+
+
+                    }
+
+                    
 
 
                 }
@@ -52,6 +67,8 @@ public class CanStackObject : MonoBehaviour
         }
 
         //}
+
+    
 
 	}
 
@@ -70,6 +87,12 @@ public class CanStackObject : MonoBehaviour
             
 
         //}
+    
+    public Vector3 multiply(Vector3 a,Vector3 b)
+    {
+
+        return new Vector3(a[0]*b[0],a[1]*b[1],a[2]*b[2]);
+    }
         
 
         
